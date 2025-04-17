fbb010cf9621adbc88c0f6fea043194a525e9fda
Nils Liebig <n.liebig@dreistrom.land>
2025-04-17 13:11:17 +0200

    [TASK] #ROC-129 added codemirror - added codemirror - introduced template structure - adjusted webpack encore settings

---
+++ assets/ts/modules/ScriptEditorHandler.ts
@@ -0,0 +1,51 @@
+import { EditorView } from '@codemirror/view';
+import { javascript } from '@codemirror/lang-javascript';
+import { basicSetup } from 'codemirror';
+
+export class ScriptEditorHandler {
+    private static instance: ScriptEditorHandler;
+    private editor?: EditorView;
+
+    public static getInstance(): ScriptEditorHandler {
+        if (!ScriptEditorHandler.instance) {
+            ScriptEditorHandler.instance = new ScriptEditorHandler();
+        }
+        return ScriptEditorHandler.instance;
+    }
+
+    public init(): void {
+        this.addCodeEditor();
+    }
+
+    private addCodeEditor(): void {
+        const editorElement = document.querySelector('.editor-container');
+
+        if (!editorElement) {
+            console.warn('[ScriptEditorHandler] Editor container not found.');
+            return;
+        }
+
+        this.editor = new EditorView({
+            doc: '#!/bin/bash',
+            extensions: [basicSetup, javascript()],
+            parent: editorElement,
+        });
+    }
+
+    public getEditorContent(): string | undefined {
+        return this.editor?.state.doc.toString();
+    }
+
+    public setEditorContent(content: string): void {
+        if (this.editor) {
+            const transaction = this.editor.state.update({
+                changes: {
+                    from: 0,
+                    to: this.editor.state.doc.length,
+                    insert: content,
+                },
+            });
+            this.editor.dispatch(transaction);
+        }
+    }
+}
